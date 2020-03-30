5ff88cd6988ace75996e07ce54513dd6aceacd11
ethanibarra <ethanibarra@localhost>
2020-03-30 12:44:25 -0600

    se añadio una linea nueva

--- src/pruebaq/Pruebaq.java
+++ src/pruebaq/Pruebaq.java
@@ -16,6 +16,7 @@ public class Pruebaq {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo y Adios Mundo");
     }
     
 }
